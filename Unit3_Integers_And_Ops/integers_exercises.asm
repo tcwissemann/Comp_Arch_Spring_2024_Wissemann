d6c867d1b44d606bb680da8169e73684333b1481
Thomas <tcwissemann@gmail.com>
2024-02-08 21:01:27 +0000

    initial commit

--- Unit3_Integers_And_Ops/integers_exercises.asm
+++ Unit3_Integers_And_Ops/integers_exercises.asm
@@ -19,11 +19,52 @@ segment .bss
 
 segment .text
         global  asm_main
+        
 asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+	; mov eax, -34	; TODO: add code
+        ; dump_regs 1
+
+        ;incorrect extension
+        ; mov eax, 0
+        ; mov al, 0DEH
+        ; call print_int
+        ; call print_nl
+
+        ;correct extension
+        ; mov eax, -1
+        ; mov al, 0DEH
+        ; call print_int
+        ; call print_nl
+
+        ; mov eax, -1
+        ; mov ax, 045H
+        ; dump_regs 1
+        ; call print_nl
+
+        ; movzx eax, ax
+        ; dump_regs 2
+
+        mov eax, prompt
+        call print_string
+
+        call read_int
+        [input], eax
+
+        imul eax
+        mov ebx, eax
+        mov eax, square_msg
+        call print_string
+        mov eax, ebx
+        call print_int
+        call print_nl
+
+        mov ebx, eax
+        imul ebx, [input]
+        mov eax, cube_msg
+        call print_string
 
         popa
         mov     eax, 0            ; return back to C
--- Unit3_Integers_And_Ops/shifts_exercises.asm
+++ Unit3_Integers_And_Ops/shifts_exercises.asm
@@ -21,7 +21,29 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+	; mov eax, 4	; TODO: add code
+        ; mov ebx, 2
+        ; test eax, ebx
+        ; dump_regs 1
+
+        ; mov eax, 13
+        ; xor eax, eax
+        ; call print_int
+
+        ; mov ebx, 1
+        ; shl ebx, 2
+
+        ; mov eax, 11
+        ; or eax, ebx
+        ; call print_int
+
+        mov ebx, 1
+        shl ebx, 3
+        not ebx
+
+        mov eax, 15
+        or eax, ebx
+        call print_int
 
         popa
         mov     eax, 0            ; return back to C
